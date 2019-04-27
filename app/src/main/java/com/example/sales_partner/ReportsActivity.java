54dce7fd6b956d58210eb6bc5ff09e37256f2186
tlacloc <eri.cas.cor@gmail.com>
2019-04-26 20:49:19 -0500

    accion de botones y cardView

--- app/src/main/java/com/example/sales_partner/ReportsActivity.java
+++ app/src/main/java/com/example/sales_partner/ReportsActivity.java
@@ -5,10 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.example.sales_partner.reports.ReportsProductsActivity;
+import com.example.sales_partner.reports.ReportsSalesActivity;
+import com.example.sales_partner.reports.ReportsSimActivity;
 
 public class ReportsActivity extends AppCompatActivity {
     
@@ -26,6 +27,27 @@ public class ReportsActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), ReportsProductsActivity.class);
+                //intent.putExtra("customer",selectedCustomer);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton simulation = findViewById(R.id.SimulationReportsBtn);
+        ImageButton ReviewReportsBtn = findViewById(R.id.ReviewReportsBtn);
+
+        simulation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ReportsSimActivity.class);
+                //intent.putExtra("customer",selectedCustomer);
+                startActivity(intent);
+            }
+        });
+
+        ReviewReportsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ReportsSalesActivity.class);
                 //intent.putExtra("customer",selectedCustomer);
                 startActivity(intent);
             }
--- app/src/main/java/com/example/sales_partner/reports/ReportsSalesActivity.java
+++ app/src/main/java/com/example/sales_partner/reports/ReportsSalesActivity.java
@@ -1,11 +1,14 @@
 package com.example.sales_partner.reports;
 
+import android.databinding.adapters.CardViewBindingAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.sales_partner.R;
 
 public class ReportsSalesActivity extends AppCompatActivity {
+
+    CardViewBindingAdapter cardViewBindingAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
